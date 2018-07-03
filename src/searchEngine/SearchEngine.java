a461e3235152bf2831c490cf1f2583aa731b66e5
rajocular <raja.ms96@gmail.com>
2018-07-03 16:13:46 -0400

    frequency() is defined to store the frequencies of every word(SE.java updated)

--- src/searchEngine/SearchEngine.java
+++ src/searchEngine/SearchEngine.java
@@ -27,8 +27,10 @@ public class SearchEngine {
 	static int freq;
 	
 	static File final_file = new File("Results.html");
+	static File freq_file = new File("Frequency.html");
 	
 	static PrintStream output_html;
+	static PrintStream print_html;
 	
 	static Hashtable<String, Integer> search_table = new Hashtable<String,Integer>();
 	static Hashtable<String, String> relate_table = new Hashtable<>();
@@ -76,7 +78,7 @@ public class SearchEngine {
 			
 			related(files,search_table,relate_table,search2);	
 		}
-	    
+	    frequency(files);
 	}
 	
 	public static int search(File[] files, Hashtable<String, Integer> search_table, String search) throws IOException
@@ -254,5 +256,65 @@ public class SearchEngine {
 		
 		
 	}
-
+	private static void frequency(File[] files)throws IOException {
+		int d=1;
+		print_html = new PrintStream(new FileOutputStream(freq_file));print_html.println("<html>\n<title>FrequencyCalculator</title><head><style>td{min-width=60px;}</style></head><body background=\"download.jpg\"><table><tr><td>File</td>");
+		while(d<4)
+		{
+			print_html.print("<td>");
+			print_html.print(d);
+			print_html.print("</td>");
+			d++;
+		}
+		for(int j =0;j<files.length;j++)
+		{
+			//System.out.println("file"+(j+1));
+			TST<Queue<Integer>> st = new TST<Queue<Integer>>();
+			int f=0;
+			try
+			{
+				BufferedReader br = new BufferedReader(new FileReader(files[j]));
+				String s = br.readLine();
+		    	while(s!=null)
+		    	{
+		    		StringTokenizer sto = new StringTokenizer(s);   
+		    		while(sto.hasMoreTokens())
+		        	{
+		    			str = sto.nextToken().toLowerCase();
+		    			if(str.matches("[a-zA-Z][a-zA-Z0-9]+"))
+						{
+		    				if(!(str.equalsIgnoreCase("what")||	str.equalsIgnoreCase("is")||str.equalsIgnoreCase("the")||str.equalsIgnoreCase("of")||str.equalsIgnoreCase("and")||str.equalsIgnoreCase("to")||str.equalsIgnoreCase("if")||str.equalsIgnoreCase("this")||str.equalsIgnoreCase("must")||str.equalsIgnoreCase("such")||str.equalsIgnoreCase("each")||str.equalsIgnoreCase("that")||str.equalsIgnoreCase("in")||str.equalsIgnoreCase("for")||str.equalsIgnoreCase("by")||str.equalsIgnoreCase("be")||str.equalsIgnoreCase("from")||str.equalsIgnoreCase("you")||str.equalsIgnoreCase("not")||str.equalsIgnoreCase("we")||str.equalsIgnoreCase("as")||str.equalsIgnoreCase("are")||str.equalsIgnoreCase("with")||str.equalsIgnoreCase("any")||str.equalsIgnoreCase("or")||str.equalsIgnoreCase("above")||str.equalsIgnoreCase("can")||str.equalsIgnoreCase("also")||	str.equalsIgnoreCase("an")||str.equalsIgnoreCase("it")||str.equalsIgnoreCase("but")||str.equalsIgnoreCase("at")||str.equalsIgnoreCase("use")||str.equalsIgnoreCase("have")||str.equalsIgnoreCase("will")||str.equalsIgnoreCase("do")||str.equalsIgnoreCase("more")||str.equalsIgnoreCase("yes")||str.equalsIgnoreCase("open")||	str.equalsIgnoreCase("may")||str.equalsIgnoreCase("which")||str.equalsIgnoreCase("on")||str.equalsIgnoreCase("all")	))
+		    				{
+							if(!st.contains(str))
+							{
+								st.put(str,new  Queue<Integer>());
+								st.get(str).enqueue(f);
+							}    
+							else
+							{
+								st.get(str).enqueue(f);
+							}
+		    				}
+						}
+		        	}	        	
+		        	s=br.readLine();
+		    	}
+				br.close();
+				
+			}
+			catch(NoSuchElementException ns) {}
+			SplayTree<String,Integer> stree = new SplayTree<String,Integer>();
+			for(String key:st.keys())
+			{
+				stree.insert(key,st.get(key).size());
+			}
+			String name = files[j].getName();
+			stree.printrange(3,name.substring(0, name.length()-4),print_html);
+			stree.makeEmpty();
+			st.makeEmpty();
+		}
+		print_html.println("</table>");
+		print_html.close();
+		
+	}
 }
